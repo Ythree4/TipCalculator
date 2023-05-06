11574dae437e99c6d27237429b41fb9d8a77e898
Ythree4 <132032713+Ythree4@users.noreply.github.com>
2023-05-06 12:04:29 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['*'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Bebas Neue', 'cursive'],
+    },
+  },
+  plugins: [],
+};
